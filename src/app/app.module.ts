53e02f4d6b792a2d8da27e1427f035d9d6bfc7f4
Francisco Calderon <fjavier5152@gmail.com>
2019-09-29 13:45:32 -0400

    Start adding layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule }       from './app-routing.module';
+import { AppComponent }           from './app.component';
+import { StoreModule }            from '@ngrx/store';
+import { HomeModule }             from './home/home.module';
+import { LayoutModule }           from './layout/layout.module';
 import { reducers, metaReducers } from './reducers';
 
 @NgModule({
@@ -13,6 +15,8 @@ import { reducers, metaReducers } from './reducers';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    LayoutModule,
+    HomeModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
       runtimeChecks: {
